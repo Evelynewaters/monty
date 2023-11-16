02c7a3aafa85249f12db99adda1dd6cac860d7e1
Evelynewaters <eveclaraify@gmail.com>
2023-11-16 14:19:51 -0800

    Docs- this commit contains solution to mandatory task and some advanced tasks

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository contains solution codes for 0x19. C - Stacks, Queues - LIFO, FIFO
---
+++ addnode.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+/**
+ * addnode - add node to head
+ * @head: head of stack
+ * @n: the new value
+ * Return: returns nothing
+*/
+void addnode(stack_t **head, int n)
+{
+
+	stack_t *new_node, *aux;
+
+	aux = *head;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{ printf("Error\n");
+		exit(0); }
+	if (aux)
+		aux->prev = new_node;
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+	*head = new_node;
+}
